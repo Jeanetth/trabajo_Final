b83029bfd1fe7c9b6ef9edc13c65c9a101834645
Jeanetth <82277025+Jeanetth@users.noreply.github.com>
2022-06-04 23:44:38 -0400

    Add files via upload

---
+++ sinhx.py
@@ -0,0 +1,27 @@
+
+import math
+class Sinxh:
+    def __init__(self, cifras, x):
+            self.cifras = cifras
+            self.x = x
+            self.aproximaciones = []
+            self.errores = []
+    @property
+    def sinxh(self):
+        #Error de tolerancia
+        Ess = 0.5 * (10**(2 - self.cifras));
+        ea = 1000
+        aproximacionActual = 0
+        n=0
+        while ea > Ess: 
+            self.aproximaciones.append(aproximacionActual)
+            aproximacionAnterior = aproximacionActual
+            aproximacionActual = aproximacionActual + (math.pow(self.x,(2*n+1))/math.factorial(2*n+1))
+            ea = abs((aproximacionActual-aproximacionAnterior)/aproximacionActual)*100
+            self.errores.append(ea)
+            n = n + 1
+        resultado = {
+            'Aproximaciones': self.aproximaciones,
+            'Errores': self.errores,
+        }
+        return resultado
