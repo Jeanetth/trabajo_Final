91bb4416d226f0e9a3a60c5e05f0c6d8e8813fa1
Jeanetth <82277025+Jeanetth@users.noreply.github.com>
2022-06-08 01:03:24 -0400

    Add files via upload

--- euler.py
+++ euler.py
@@ -35,22 +35,6 @@ class Euler:
                 }
     return resultado
 #TEST 
-"""
-Se prueba la EDO y'=2xy sabiendo que y(0)=1 y queremos encontrar la y(0.5)
-formando un intervalo de 0.1 0.2 0.3 0.4 0.5 es decir con h=0.1 
 
-Los parametros van asi 
-
-la x inicial = 0
-la y inicial = 1  -----> se toman de y(0)=1
-la x fina = 0.5 ------> se toma de y(0.5)
-el h ---> cuyo intervalor se toma 
-
-Referencia 1 del ejercicio Unidad 5 --> EDO_e_inicio_de_Euler.pdf --> pagina 9 la ingeniera
-presenta un error en la realizacion del ejercicio 
-
-Referencia 1 libro CÁLCULO NUMÉRICO Libro de Cátedra R. RIVEROS, pagina 276
-
-"""
 resultado = Euler(0,1,0.5,0.1,"2*x*y")
 print(resultado.euler())
